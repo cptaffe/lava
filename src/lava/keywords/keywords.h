3fe3e3412dab8e57ed8c2dac36f324b7e4d75deb
Connor Taffe <cpaynetaffe@gmail.com>
2014-10-19 15:23:02 -0500

    FirstPass now generates hash map of async futures for evaluating defines

---
+++ src/lava/keywords/keywords.h
@@ -0,0 +1,13 @@
+
+#ifndef LAVA_LAVA_KEYWORDS_KEYWORDS_H_
+#define LAVA_LAVA_KEYWORDS_KEYWORDS_H_
+
+namespace lava {
+	const std::string KEYWORD_DEF = "def";
+	const std::string KEYWORD_ADD = "+";
+	const std::string KEYWORD_SUB = "-";
+	const std::string KEYWORD_MUL = "*";
+	const std::string KEYWORD_DIV = "/";
+}
+
+#endif // LAVA_LAVA_KEYWORDS_KEYWORDS_H_
